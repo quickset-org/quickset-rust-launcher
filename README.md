bbf971cf943baf3dd1d741b5f9d5f0b0113373e8
Jared Dobson <jared.dobson@live.com>
2021-01-17 20:29:21 -0700

    Broken checkpoint

---
+++ README.md
@@ -0,0 +1,11 @@
+# Quickset
+
+```bash
+$ quickset
+Quickset v0.0.1 found local quickset file would you like to import it? [y/N]
+
+$ quickset 
+Quickset v0.0.1 importing .quickset.toml
+Downloading []
+
+```
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+//extern crate diesel;
+
+#[macro_use]
+extern crate diesel_migrations;
+embed_migrations!("../migrations");
+
 fn main() {
     let operation = std::env::args().nth(1).expect("no pattern given");
     let project = std::env::args().nth(2).expect("no path given");
