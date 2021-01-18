f205f4f3dc7ae85d642d2f27952dee01ecdf5d07
Jared Dobson <jared.dobson@live.com>
2021-01-17 18:15:58 -0700

    'initial'

---
+++ src/main.rs
@@ -0,0 +1,6 @@
+fn main() {
+    let operation = std::env::args().nth(1).expect("no pattern given");
+    let project = std::env::args().nth(2).expect("no path given");
+    println!("Quickset v0.0.1");
+    println!("Operation: {operation} - Project: {project}", operation = operation, project = project);
+}
